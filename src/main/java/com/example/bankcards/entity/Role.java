20d9767dee9eadc98a823183bf4d1db26d26a852
Vladimir <eurasian@list.ru>
2025-08-30 13:32:41 +0300

    STEP-1 DONE

---
+++ src/main/java/com/example/bankcards/entity/Role.java
@@ -0,0 +1,3 @@
+package com.example.bankcards.entity;
+
+public enum Role {ADMIN, USER}
---
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 64)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<Role> roles = new HashSet<>();
+
+
+
+}
